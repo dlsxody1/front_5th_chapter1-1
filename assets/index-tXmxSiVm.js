cf46126096b20142b8de107217dc330eea1fc60f
dlsxody1 <dlsxody1@naver.com>
2025-03-27 20:21:14 +0900

    Updates

--- assets/index-tXmxSiVm.js
+++ assets/index-tXmxSiVm.js
@@ -1,19 +1,19 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerPolicy&&(r.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?r.credentials="include":s.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(s){if(s.ep)return;s.ep=!0;const r=o(s);fetch(s.href,r)}})();class y{constructor(t){this.routes=t,this.container=document.body.querySelector("#root")}render(t=window.location.pathname){(this.routes[t]||this.routes.default).render(this.container)}start(){this.render(),window.addEventListener("popstate",()=>this.render())}navigate(t){t!==window.location.pathname&&(history.pushState({},"",t),this.render(t))}}const c=()=>`<header class="bg-blue-600 text-white p-4 sticky top-0">
+(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const l of s)if(l.type==="childList")for(const a of l.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function r(s){const l={};return s.integrity&&(l.integrity=s.integrity),s.referrerPolicy&&(l.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?l.credentials="include":s.crossOrigin==="anonymous"?l.credentials="omit":l.credentials="same-origin",l}function o(s){if(s.ep)return;s.ep=!0;const l=r(s);fetch(s.href,l)}})();class v{constructor(e){this.routes=e,this.container=document.body.querySelector("#root")}render(e=window.location.pathname){if(this.routes[e]){this.routes[e].render(this.container);return}let r=e;window.BASE_ROUTE&&e.startsWith(window.BASE_ROUTE)&&(r=e.slice(window.BASE_ROUTE.length)||"/");const o=`${window.BASE_ROUTE}${r==="/"?"":r}`;(this.routes[o]||this.routes.default).render(this.container)}start(){this.render(),window.addEventListener("popstate",()=>this.render())}navigate(e){const r=e.startsWith("/")?e:`/${e}`,o=window.BASE_ROUTE+r;o!==window.location.pathname&&(history.pushState({},"",o),this.render(o))}}const c=()=>`<header class="bg-blue-600 text-white p-4 sticky top-0">
           <h1 class="text-2xl font-bold">항해플러스</h1>
-        </header>`,l={state:{user:JSON.parse(localStorage.getItem("user")),isLoggedIn:!!localStorage.getItem("user")},listeners:[],subscribe(e){return this.listeners.push(e),()=>{this.listeners=this.listeners.filter(t=>t!==e)}},getState(){return this.state},setState(e){this.state={...this.state,...e},e.user&&localStorage.setItem("user",JSON.stringify(this.state.user)),this.listeners.forEach(t=>t(this.state))},actions:{login:(e,t)=>{l.setState({user:{...l.state.user,username:e,email:t,bio:""},isLoggedIn:!0})},logout:()=>{localStorage.removeItem("user"),l.setState({isLoggedIn:!1}),l.state.user={username:"",email:"",bio:""}},updateProfile:e=>{l.setState({user:{...l.state.user,...e}})}}},f=()=>{const t=l.getState().isLoggedIn,o=window.location.hash||"#/",a=s=>`#${s}`;return`
+        </header>`,i={state:{user:JSON.parse(localStorage.getItem("user")),isLoggedIn:!!localStorage.getItem("user")},listeners:[],subscribe(t){return this.listeners.push(t),()=>{this.listeners=this.listeners.filter(e=>e!==t)}},getState(){return this.state},setState(t){this.state={...this.state,...t},t.user&&localStorage.setItem("user",JSON.stringify(this.state.user)),this.listeners.forEach(e=>e(this.state))},actions:{login:(t,e)=>{i.setState({user:{...i.state.user,username:t,email:e,bio:""},isLoggedIn:!0})},logout:()=>{localStorage.removeItem("user"),i.setState({isLoggedIn:!1}),i.state.user={username:"",email:"",bio:""}},updateProfile:t=>{i.setState({user:{...i.state.user,...t}})}}},f=()=>{const e=i.getState().isLoggedIn,r=window.location.hash||"#/",o=s=>`#${s}`;return`
     <nav class="bg-white shadow-md p-2 sticky top-0">
         <ul class="flex justify-around">
-          <li><a href="${a("/")}" class="${o==="#/"?"text-blue-600 font-bold":"text-gray-600"}">홈</a></li>
-          ${t?`<li><a href="${a("/profile")}" class="${o==="#/profile"?"text-blue-600 font-bold":"text-gray-600"}">프로필</a></li>
+          <li><a href="${o("/")}" class="${r==="#/"?"text-blue-600 font-bold":"text-gray-600"}">홈</a></li>
+          ${e?`<li><a href="${o("/profile")}" class="${r==="#/profile"?"text-blue-600 font-bold":"text-gray-600"}">프로필</a></li>
                  <li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>`:`<li>
                    <a href="/login" class="text-gray-600 hidden">로그인</a>
-                   <a href="${a("/login")}" data-testid="login-link" class="${o==="#/login"?"text-blue-600 font-bold":"text-gray-600"}">로그인</a>
+                   <a href="${o("/login")}" data-testid="login-link" class="${r==="#/login"?"text-blue-600 font-bold":"text-gray-600"}">로그인</a>
                  </li>`}
         </ul>
       </nav>
-    `},m=()=>{document.body.addEventListener("click",e=>{const t=e.target.closest("a");if(t){if(t.id==="logout"){e.preventDefault(),l.actions.logout(),window.router.navigate("/login");return}if(t.getAttribute("href").startsWith("#")){e.preventDefault();const o=t.getAttribute("href").slice(1);window.router.navigate(o)}}})},g=()=>`<footer class="bg-gray-200 p-4 text-center">
+    `},m=()=>{document.body.addEventListener("click",t=>{const e=t.target.closest("a");if(e){if(e.id==="logout"){t.preventDefault(),i.actions.logout(),window.router.navigate("/login");return}if(e.getAttribute("href").startsWith("#")){t.preventDefault();const r=e.getAttribute("href").slice(1);window.router.navigate(r)}}})},g=()=>`<footer class="bg-gray-200 p-4 text-center">
         <p>&copy; 2024 항해플러스. All rights reserved.</p>
-      </footer>`,L=e=>{e&&(e.innerHTML=`
+      </footer>`,y=t=>{t&&(t.innerHTML=`
   <div class="bg-gray-100 min-h-screen flex justify-center">
     <div class="max-w-md w-full">
     
@@ -50,7 +50,7 @@ ${g()}
    
     </div>
   </div>
-`,m())},S=e=>{if(!e)return;if(l.getState().isLoggedIn){window.router.navigate("/");return}e.innerHTML=`
+`,m())},E=t=>{if(!t)return;if(i.getState().isLoggedIn){window.router.navigate("/");return}t.innerHTML=`
     <main class="bg-gray-100 flex items-center justify-center min-h-screen">
       <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
@@ -75,7 +75,7 @@ ${g()}
         </div>
       </div>
     </main>
-    `,document.getElementById("login-form").addEventListener("submit",o=>{o.preventDefault();const a=document.getElementById("username").value;if(a.length===0)return alert("아이디 혹은 비밀번호를 입력해주세요!");l.actions.login(a,""),console.log(l.getState().isLoggedIn,"login 상태확인"),window.router.navigate("/")})},I=e=>{if(!e)return;const t=localStorage.getItem("user");if(!t){window.router.navigate("/login");return}const o=JSON.parse(t),a=o.username||"",s=o.email||"";let r=o.bio||"";r==="자기소개입니다."&&!r.includes("자기소개입니다. 자기소개입니다.")&&(r="자기소개입니다. 자기소개입니다."),e.innerHTML=`
+    `,document.getElementById("login-form").addEventListener("submit",r=>{r.preventDefault();const o=document.getElementById("username").value;if(o.length===0)return alert("아이디 혹은 비밀번호를 입력해주세요!");i.actions.login(o,""),console.log(i.getState().isLoggedIn,"login 상태확인"),window.router.navigate("/")})},S=t=>{if(!t)return;const e=localStorage.getItem("user");if(!e){window.router.navigate("/login");return}const r=JSON.parse(e),o=r.username||"",s=r.email||"";let l=r.bio||"";l==="자기소개입니다."&&!l.includes("자기소개입니다. 자기소개입니다.")&&(l="자기소개입니다. 자기소개입니다."),t.innerHTML=`
   <div class="bg-gray-100 min-h-screen flex justify-center">
     <div class="max-w-md w-full">
 ${c()}
@@ -97,7 +97,7 @@ ${c()}
                 type="text"
                 id="username"
                 name="username"
-                value="${a}"
+                value="${o}"
                 class="w-full p-2 border rounded"
                 aria-label="사용자 이름"
               />
@@ -129,7 +129,7 @@ ${c()}
                 rows="4"
                 class="w-full p-2 border rounded"
                 aria-label="자기소개"
-              >${r}</textarea>
+              >${l}</textarea>
             </div>
             <button
               type="submit"
@@ -144,7 +144,7 @@ ${c()}
       ${g()}
     </div>
   </div>
-`,m(),document.getElementById("profile-form").addEventListener("submit",h=>{h.preventDefault();const x=document.getElementById("username").value,v=document.getElementById("email").value,d=document.getElementById("bio").value;let u=d;d==="자기소개입니다."&&(u="자기소개입니다. 자기소개입니다.");const w={username:x,email:v,bio:u};l.actions.updateProfile(w),alert("프로필이 업데이트되었습니다.")})},$=e=>{e&&(e.innerHTML=`
+`,m(),document.getElementById("profile-form").addEventListener("submit",p=>{p.preventDefault();const h=document.getElementById("username").value,w=document.getElementById("email").value,d=document.getElementById("bio").value;let u=d;d==="자기소개입니다."&&(u="자기소개입니다. 자기소개입니다.");const x={username:h,email:w,bio:u};i.actions.updateProfile(x),alert("프로필이 업데이트되었습니다.")})},L=t=>{t&&(t.innerHTML=`
   <main class="bg-gray-100 flex items-center justify-center min-h-screen">
     <div class="bg-white p-8 rounded-lg shadow-md w-full text-center" style="max-width: 480px">
       <h1 class="text-2xl font-bold text-blue-600 mb-4">항해플러스</h1>
@@ -158,4 +158,4 @@ ${c()}
       </a>
     </div>
   </main>
-`)},b="/front_5th_chapter1-1",i=e=>`${b}${e}`,E={[i("/")]:{render:L},[i("/login")]:{render:S},[i("/profile")]:{render:I},default:{render:$}},p=new y(E);window.router=p;window.BASE_ROUTE=b;p.start();
+`)},n="/front_5th_chapter1-1";console.log(n,"base");const $={[`${n}/`]:{render:y},[`${n}/login`]:{render:E},[`${n}/profile`]:{render:S},default:{render:L}},b=new v($);window.router=b;window.BASE_ROUTE=n;b.start();
